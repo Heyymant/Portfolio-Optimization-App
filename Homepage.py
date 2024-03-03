598838271142da54cc04c1bd586303cc919757d3
Heyymant <hemant_03@iitb.ac.in>
2024-03-03 19:48:19 +0530

    fixed errors

--- Homepage.py
+++ Homepage.py
@@ -7,9 +7,9 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import yfinance as yf
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import plotly.graph_objects as go
-import plotly.figure_factory as ff
+# import plotly.figure_factory as ff
 
 from pypfopt.efficient_frontier import EfficientFrontier
 from pypfopt import risk_models
@@ -22,7 +22,7 @@ import plotly.express as px
 import seaborn as sns
 from datetime import datetime
 from io import BytesIO
-plt.style.use('fivethirtyeight')
+# plt.style.use('fivethirtyeight')
 
 @st.cache_data
 def bg_png(png):
